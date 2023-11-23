e2c212a54f9e49e015417840c2d74da1c45e5958
Luka Jovanovic <lukaj2501@gmail.com>
2023-11-22 19:48:12 -0500

    tinkering with shaders

--- testproj/main.cpp
+++ testproj/main.cpp
@@ -1,4 +1,4 @@
-#include <SDL.h>
+#include <SDL2.h>
 #include <stdio.h>
 #include <typeinfo>
 #include "graphics.h"
---
+++ testshaders/RUN.bat
@@ -0,0 +1,1 @@
+g++ main.cpp -IC:\cpp_libs\SDL\include\SDL2 -LC:\cpp_libs\SDL\lib -lmingw32 -lSDL2main -lSDL2 -lGL -o run && run
