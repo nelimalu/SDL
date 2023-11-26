d4bd708ef0cc5bd808c8875d09276f0521bc3ca2
Luka Jovanovic <lukaj2501@gmail.com>
2023-11-26 16:16:48 -0500

    sdl-gpu working main loop

--- testshaders/main.cpp
+++ testshaders/main.cpp
@@ -3,7 +3,26 @@
 
 
 int main(int argc, char *argv[]) {
-	GPU_Target* win = GPU_Init(800, 600, 0);
+	GPU_Target* screen = GPU_Init(800, 600, GPU_DEFAULT_INIT_FLAGS);
+
+	if (screen == NULL)
+		return 3;
+
+
+	SDL_Event event;
+	bool run = true;
+	while (run) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT)
+				run = false;
+		}
+
+		GPU_ClearRGBA(screen, 240, 240, 240, 255);
+		GPU_Flip(screen);
+
+	}
+
+	GPU_Quit();
 
 	return 0;
 }
